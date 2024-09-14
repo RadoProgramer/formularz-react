77b5dcd8a972311c90921c2e5c1863e7bb343e0b
RadoProgramer <adekkubacki@gmail.com>
2024-09-14 12:03:05 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,55 @@
 //import "/App.css";
 
+
+//Formularz niekontrolowany
+
+
+// function App() {
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     const { login, password } = event.target.elements;
+//     console.log("submiting...", login.value, password.value);
+//     event.target.reset();
+    
+//   };
+//   return (
+//     <>
+//       <form onSubmit={handleSubmit}>
+//         <input name="login" />
+//         <input name="password" />
+//         <button type="submit">Wyślij</button>
+//       </form>
+//     </>
+//   );
+// }
+// export default App;
+
+
+//formularz kontrolowany
+
 function App() {
+  const [login, setLogin] = useState("login val");
+  const [password, setPassword] = useState("pass val");
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    const { login, password } = event.target.elements;
-    console.log("submiting...", login.value, password.value);
-    event.target.reset();
+    console.log(login, password);
     
   };
+
+  const handleLoginChange = (e) => {
+    setLogin(e.target.value);
+  };
+
+  const handlePassChange = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input name="login" />
-        <input name="password" />
+        <input name="login" value={login} onChange={handleLoginChange} />
+        <input name="password" value={password} onChange={handlePassChange} />
         <button type="submit">Wyślij</button>
       </form>
     </>
